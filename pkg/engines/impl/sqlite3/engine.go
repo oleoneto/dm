6acd1cbcb69db50eec0c4cb45bbb7245f3597cfe
Leo <oleoneto@users.noreply.github.com>
2024-05-17 19:03:19 -0300

    Update engine.go

--- pkg/engines/impl/sqlite3/engine.go
+++ pkg/engines/impl/sqlite3/engine.go
@@ -7,18 +7,18 @@ import (
 
 type SQLite3 struct{ db *sql.DB }
 
-func (e *SQLite3) Exec(context.Context, string, ...any) (sql.Result, error) {
+func (e *SQLite3) Exec(ctx context.Context, query string, args ...any) (sql.Result, error) {
     r, err := e.db.ExecContext(ctx, query, args)
     return r, err
 }
 
-func (e *SQLite3) Query(context.Context, string, ...any) (*sql.Rows, error) {
+func (e *SQLite3) Query(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
     r, err := e.db.QueryContext(ctx, query, args)
     return r, err
 }
 
 
-func (e *SQLite3) QueryRow(context.Context, string, ...any) (*sql.Row, error) {
+func (e *SQLite3) QueryRow(ctx context.Context, query string, args ...any) (*sql.Row, error) {
     r := e.db.QueryRowContext(ctx, query, args)
     return r, nil
 }
