111fe63cc08eb0833ab00c4f8e50208526795418
Leo <oleoneto@users.noreply.github.com>
2024-05-17 18:49:46 -0300

    Update postgresql/engine.go

--- pkg/engines/impl/postgresql/engine.go
+++ pkg/engines/impl/postgresql/engine.go
@@ -5,10 +5,21 @@ import (
 	"database/sql"
 )
 
-type PostgreSQL struct{}
+type PostgreSQL struct{
+    db *sql.DB
+}
 
-func (e *PostgreSQL) Exec(context.Context, string, ...any) (sql.Result, error) { return nil, nil }
+func (e *PostgreSQL) Exec(ctx context.Context, query string, args ...any) (sql.Result, error) { 
+    r, err := e.db.ExecContext(ctx, query, args)
+    return r, err
+}
 
-func (e *PostgreSQL) Query(context.Context, string, ...any) (*sql.Rows, error) { return nil, nil }
+func (e *PostgreSQL) Query(ctx context.Context, query string, args ...any) (*sql.Rows, error) { 
+    r, err := e.db.QueryContext(ctx, query, args)
+    return r, err
+}
 
-func (e *PostgreSQL) QueryRow(context.Context, string, ...any) *sql.Row { return nil }
+func (e *PostgreSQL) QueryRow(ctx context.Context, query string, args ...any) (*sql.Row, error) {
+    r := e.db.QueryRowContext(ctx, query, args)
+    return r, nil
+}
